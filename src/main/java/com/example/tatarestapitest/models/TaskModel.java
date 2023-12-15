a48cb60ea0042853d7bdbf32f62bee3b6aaa2e26
Francisco Ezequiel Baños Ramirez <chekelosos@gmail.com>
2023-12-14 21:38:09 -0600

    Added configuration to acces the H2 database and added the model for Task.

---
+++ src/main/java/com/example/tatarestapitest/models/TaskModel.java
@@ -0,0 +1,71 @@
+package com.example.tatarestapitest.models;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String name;
+    private String title;
+    private String description;
+    private LocalDate creationDate;
+    private boolean completed;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+}
